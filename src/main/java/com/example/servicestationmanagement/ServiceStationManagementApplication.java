71bbbc1d8f1c79d7697927c828a69927b8288e9a
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-05-07 13:23:02 +0300

    initialize Service Station Management project for PROJET1

---
+++ src/main/java/com/example/servicestationmanagement/ServiceStationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicestationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceStationManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceStationManagementApplication.class, args);
+    }
+
+}
